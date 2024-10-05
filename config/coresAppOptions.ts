4c4f8d754dc29493994152befb3ed0091e66a8d2
guyzoum77 <guyzoum7700@mail.com>
2024-10-05 20:31:10 +0000

    changing architecture

---
+++ config/coresAppOptions.ts
@@ -0,0 +1,4 @@
+export const coresAppOptions = {
+    corsOptions: {},
+    optionsUrlencoded: {}
+}
--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import {Kernel} from "../app/core/kernel";
+import {Kernel} from "./app/core/kernel";
 import {KernelModules} from "opticore-core-module";
 
 (async(): Promise<void> => { await KernelModules(Kernel()); })();
